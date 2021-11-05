66a39f0c384efc88eab6cb43535251ec66b3584c
Ivan <lubovdobro@gmail.com>
2021-11-05 20:16:31 +0300

    -

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -9,27 +9,52 @@
 
 class IMoveble;
 
-std::shared_ptr<UObject> createMovable()
+std::shared_ptr<UObject> createMovable(int xCoord, int yCoord, int xV, int yV)
 {
     auto moveble  = std::make_shared<UObject>();
 
     auto coord =  Eigen::RowVector2i(); 
-    coord(0) = 12;
-    coord(1) = 5;
+    coord(0) = xCoord;
+    coord(1) = yCoord;
     moveble->setProperty("coordinates", coord);
 
     auto velocity =  Eigen::RowVector2i(); 
-    velocity(0) = -7;
-    velocity(1) = 3;
+    velocity(0) = xV;
+    velocity(1) = yV;
     moveble->setProperty("velocity", velocity);
 
     return moveble;
 }
 
 
+std::shared_ptr<UObject> createMovable(int xV, int yV)
+{
+    auto moveble  = std::make_shared<UObject>();
+
+    // auto coord =  Eigen::RowVector2i(); 
+    // coord(0) = xCoord;
+    // coord(1) = yCoord;
+    // moveble->setProperty("coordinates", coord);
+
+    auto velocity =  Eigen::RowVector2i(); 
+    velocity(0) = xV;
+    velocity(1) = yV;
+    moveble->setProperty("velocity", velocity);
+
+    return moveble;
+}
+
+
+
 std::shared_ptr<IMovable> createAdapter()
 {
-   return std::make_shared<MovebleAdapter>(createMovable());
+   return std::make_shared<MovebleAdapter>(createMovable(12, 5, -7, 3));
+}
+
+
+std::shared_ptr<IMovable> createBadAdapter()
+{
+   return std::make_shared<MovebleAdapter>(createMovable(-7, 3));
 }
 
 
@@ -47,6 +72,25 @@ TEST(UObjectTest, SetProperty)
     test(1) = 8;
 
 
+    EXPECT_EQ(res, test);
+
+    delete command;
+}
+
+
+TEST(UObjectTest, BadProperty)
+{
+    auto testObject = createBadAdapter();
+    auto command = new MoveCommand(testObject);
+
+    command->execute();
+
+    auto res = testObject->getCoordinates();
+
+    auto test = Eigen::RowVector2i(); 
+    test(0) = 5;
+    test(1) = 8;
+    
     EXPECT_EQ(res, test);
 
     delete command;
